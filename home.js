dbdd901792c49bd52a45080c236c0edbbfe8a1af
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-15 19:50:08 +0530

    1

--- home.js
+++ home.js
@@ -7,10 +7,11 @@ console.log(b);
 var sum = 50;
 console.log(sum);
 
-//getting html id and changing in js
-var age = prompt("You'r age: ");
-document.getElementById('abcd').innerHTML = age;
+// // getting html id and changing in js
+// var age = prompt("You'r age: ");
+// document.getElementById('abcd').innerHTML = age;
 
 
 //Number is js
 var num1 = 5.7;
+console.log(80*98);
