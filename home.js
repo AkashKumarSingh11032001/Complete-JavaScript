203fd2b9a84d4de4f9b6d35a5d3b9788954299d6
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-15 19:36:33 +0530

    1

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
 console.log("Hello World!");
-console.log("Hello World!");
-alert('Hello Hero')
+
+//variable
+var b = 'smoothie';
+console.log(b);
+
+var sum = 50;
+console.log(sum);
+
+//getting html id and changing in js
+document.getElementById('a').innerHTML = "Fuck World";
