d8c267bd455839ab3c171bad1c1d45253a3a3494
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-16 11:57:00 +0530

    string

--- strings.js
+++ strings.js
@@ -3,3 +3,27 @@ console.log(name[3]);
 console.log(name.length);
 
 console.log(name[name.length-1]); //last character
+
+let nam = "   AkasH   ";
+console.log(nam);
+
+// remiove unwanted space
+nam = nam.trim();
+console.log(nam);
+
+// Uppercase
+console.log(nam.toUpperCase());
+
+// Lowercase
+console.log(nam.toLowerCase());
+
+// slice
+console.log(nam.slice(2,5));
+
+// 
+
+
+
+
+
+
