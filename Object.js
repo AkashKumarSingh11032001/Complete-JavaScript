261401d76e96720478fcc6b11f3161eb07909f00
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-21 16:09:45 +0530

    JS

--- Object.js
+++ Object.js
@@ -6,3 +6,24 @@ console.log(person.name);
 console.log(person.age);
 
 // other ways
+
+const person ={
+    name:'akash',
+    age:21,
+    hobbies : ["a","b","c"],
+    
+}
+console.log(person);
+console.log(person.name);
+console.log(person.hobbies)
+
+// loop in object
+
+for(let key in person){
+    console.log(person[key]);
+}
+
+// object.keys(person) will give array of all keys
+for(let i of Object.keys(person)){
+    console.log(person[i])
+}
