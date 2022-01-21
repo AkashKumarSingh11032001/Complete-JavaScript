f7f564bb90aa2ded24c02d619bfae902ba835b20
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-21 15:59:03 +0530

    JS

---
+++ Object.js
@@ -0,0 +1,8 @@
+//  object data type just like dictonary in python
+
+const person = {name:'akash',age:21};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+// other ways
