74eca2e6eb4455e5775c5d2626ded5b432637ff2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-22 23:14:45 +0530

    JS

---
+++ Function.js
@@ -0,0 +1,18 @@
+// Function declaration
+
+function sing(){
+    console.log("Fuck the world! everyday")
+}
+
+sing();
+
+// 2nd type using => opr
+// function expression
+const iseven = (number) => {
+    return number%2===0;
+}
+iseven(4);
+
+
+
+
