5b67c818c437320274141b219f42ad5782dbaed7
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-23 10:46:37 +0530

    JS

--- Function.js
+++ Function.js
@@ -13,6 +13,28 @@ const iseven = (number) => {
 }
 iseven(4);
 
+// ***************************
+// param destruction
+//  object + react
 
+const person = {
+    name : "akash",
+    age : 21,
+}
+
+// without destructing
+function detail(obj){
+    console.log(obj.name);
+    console.log(obj.age);
+}
+
+// with destruction 
+function detail2({name,age}){
+    console.log(name);
+    console.log(age);
+}
+
+detail(person);
+detail2(person);
 
 
