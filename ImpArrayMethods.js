83bce21779fa4582209bff18d44561a22fd69bf2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-23 16:51:33 +0530

    JS

--- ImpArrayMethods.js
+++ ImpArrayMethods.js
@@ -38,6 +38,7 @@ const updatesq = numbers.map(function(num){
 console.log(updatesq)
 
 //  FILTERS : store the data in an array only if the mentioned condition return true only.
+
 const numbers = [3,4,5,6,7];
 const isEven = function(num){
     return num%2===0;
