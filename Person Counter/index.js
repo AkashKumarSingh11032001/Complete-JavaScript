d74465ae807361a2e8ccf1e119444abcaac33a48
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-26 14:01:12 +0530

    JS

--- Person Counter/index.js
+++ Person Counter/index.js
@@ -1,18 +1,21 @@
 
 let count = 0;
 function increment(){
-    count = count+1;
+    count += 1;
     document.getElementById("count-el").innerHTML = count;
 }
 
 function decrement(){
-    count = count-1;
+    count -= 1;
     if(count < 0){
-        document.getElementById("count-el").innerHTML = 0;
+        count = 0;
+    }else{
+        document.getElementById("count-el").innerHTML = count;
     }
 }
 
 function save(){
-    
+    let curr_messg = document.getElementById("para")
+    curr_messg.innerHTML += count + "-"
 }
 
