9e9429562d27197de05896efb9c64d1fecee6dac
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-28 23:08:26 +0530

    JS

--- Black Jack/index.js
+++ Black Jack/index.js
@@ -1,18 +1,42 @@
-let firstCard = math.R
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById('message-el')
 let sumEL = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
+let playerEl = document.getElementById('player-el')
+
+let player = {
+    name : "Akash",
+    chip : 145 
+}
+playerEl.textContent = player.name + ": $" + player.chip
 
 function start() {
+    isAlive = true
+    let firstCard = randomNumber()
+    let secondCard = randomNumber()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderstart()
 }
+
+function randomNumber() {
+    let x = Math.floor(Math.random() * 13) + 1
+    if(x === 1){
+        return 11
+    }else if(x > 10 ){
+        return 10
+    }
+    else{
+        return x
+    }
+}
+
 
 function renderstart() {
 
@@ -29,6 +53,7 @@ function renderstart() {
     messageEl.textContent = message
     sumEL.textContent = "Sum : " + sum
     // cardsEl.textContent = "Card :" + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Card :"
     for(let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
@@ -36,8 +61,13 @@ function renderstart() {
 
 function newCard() {
 
-    let card = 7
+    if(isAlive == true && hasBlackJack == false){
+    let card = randomNumber()
+    sum += card
     cards.push(card)
     renderstart()
+
+    }
+    
 }
 
