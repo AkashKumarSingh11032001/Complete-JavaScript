610fbbc64cd2dd580a54dd05e17c0d4c55952b60
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-28 18:29:43 +0530

    JS

--- Black Jack/index.js
+++ Black Jack/index.js
@@ -1,5 +1,6 @@
-let firstCard = 10
+let firstCard = math.R
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,6 +11,10 @@ let sumEL = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 
 function start() {
+    renderstart()
+}
+
+function renderstart() {
 
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -23,10 +28,16 @@ function start() {
     
     messageEl.textContent = message
     sumEL.textContent = "Sum : " + sum
-    cardsEl.textContent = "Card :" + firstCard + " " + secondCard
+    // cardsEl.textContent = "Card :" + cards[0] + " " + cards[1]
+    for(let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
 }
 
 function newCard() {
 
+    let card = 7
+    cards.push(card)
+    renderstart()
 }
 
