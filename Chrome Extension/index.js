7da0e90208e85a0737014480e32f149189a608ac
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-29 22:56:21 +0530

    JS

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -7,19 +7,25 @@ const ulEl = document.getElementById('ul-el')
 // save button functinality
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    inputEl.value = ''
+    inputEl.value = ""
+    renderLeads()
 })
 
+// show element on screan every time when click on save button
+function renderLeads(){
 
+    let listItemEl = ""
 
-let listItemEl = ""
+    // render the list content on screan
+    for(let i = 0; i < myLeads.length; i++){
+        listItemEl += "<li>" + myLeads[i] + "</li>"
+        // OR
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItemEl
 
-// render the list content on screan
-for(let i = 0; i < myLeads.length; i++){
-    listItemEl += "<li>" + myLeads[i] + "</li>"
-    // OR
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
 }
-ulEl.innerHTML = listItemEl
+
+
