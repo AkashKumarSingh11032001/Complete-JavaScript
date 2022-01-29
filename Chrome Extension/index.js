d051be4f874fe1ae3b6aad533088cc8043d82e34
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-29 22:50:25 +0530

    JS

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,16 +1,20 @@
 
-let myLeads = []
+let myLeads = [a,b,c]
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
 // save button functinality
-inputBtn.addEventListener('click', function(){
+inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ''
 })
 
 // render the list content on screan
 for(let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // OR
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
 }
