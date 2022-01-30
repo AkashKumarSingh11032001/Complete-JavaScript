8cf75e6d4587d40ab2d83ee1b3d3b2023b91ba4b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-30 23:00:22 +0530

    JS

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -4,6 +4,10 @@ const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
 // save button functinality
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
