cafecbfb61df7b0dcb0c1b4730676ca5afa5b5c3
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-01 22:21:13 +0530

    JS

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,68 +1,47 @@
-
 let myLeads = []
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const ulEl = document.getElementById('ul-el')
-const deleteBtn = document.getElementById('delete-btn')
-const tabBtn = document.getElementById('tab-btn')
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-// saving saved data to locally
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-if(leadsFromLocalStorage){
+if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// save button functinality
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem('myLeads', JSON.stringify(myLeads)) //feteching data from local strage
-    render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
-// delete button
-deleteBtn.addEventListener("dblclick",function(){
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-// save tab button functinality
-tabBtn.addEventListener("click",function(){
-    // grab url from current tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem('myLeads', JSON.stringify(myLeads)) //feteching data from local strage
-        render(myLeads)
-       
-    })
-    
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
-
-
-// show element on screan every time when click on save button
-function render(leads){
-
-    let listItemEl = ""
-
-    // render the list content on screan
-    for(let i = 0; i < leads.length; i++){
-        // listItemEl += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        // template string
-        listItemEl += `<li>
-            <a target='_blank' href=' ${leads[i]}'>
-            ${leads[i]}
-            </a>
-        </li>
-        `
-        // OR
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-    }
-    ulEl.innerHTML = listItemEl
-
-}
-
-
